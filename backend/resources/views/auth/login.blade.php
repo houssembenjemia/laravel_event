99500db0b77660fb08eb093cacdab5d140ee0279
houssembenjemia <houssembenjemia@gmail.com>
2019-12-06 20:14:45 +0100

    authentification

--- backend/resources/views/auth/login.blade.php
+++ backend/resources/views/auth/login.blade.php
@@ -1,173 +1,134 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
-<!DOCTYPE html>
 <html lang="en">
-  
-<!-- Mirrored from www.bootstrapdash.com/demo/azia/v1.0.0/template/page-signin.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 06 Dec 2019 10:24:09 GMT -->
-<head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-90680653-2"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-90680653-2');
-    </script>
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Twitter -->
-    <!-- <meta name="twitter:site" content="@bootstrapdash">
-    <meta name="twitter:creator" content="@bootstrapdash">
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:title" content="Azia">
-    <meta name="twitter:description" content="Responsive Bootstrap 4 Dashboard Template">
-    <meta name="twitter:image" content="https://www.bootstrapdash.com/azia/img/azia-social.png"> -->
-
-    <!-- Facebook -->
-    <!-- <meta property="og:url" content="https://www.bootstrapdash.com/azia">
-    <meta property="og:title" content="Azia">
-    <meta property="og:description" content="Responsive Bootstrap 4 Dashboard Template">
-
-    <meta property="og:image" content="https://www.bootstrapdash.com/azia/img/azia-social.png">
-    <meta property="og:image:secure_url" content="https://www.bootstrapdash.com/azia/img/azia-social.png">
-    <meta property="og:image:type" content="image/png">
-    <meta property="og:image:width" content="1200">
-    <meta property="og:image:height" content="600"> -->
-
-    <!-- Meta -->
-    <meta name="description" content="Responsive Bootstrap 4 Dashboard Template">
-    <meta name="author" content="BootstrapDash">
-
-    <title>Azia Responsive Bootstrap 4 Dashboard Template</title>
-
-    <!-- vendor css -->
-    <link href="../lib/fontawesome-free/css/all.min.css" rel="stylesheet">
+       
+       
+    
+        <title>Connexion</title>
+    
+        <!-- vendor css -->
+        <link href="../lib/fontawesome-free/css/all.min.css" rel="stylesheet">
     <link href="../lib/ionicons/css/ionicons.min.css" rel="stylesheet">
     <link href="../lib/typicons.font/typicons.css" rel="stylesheet">
+      
+        <!-- azia CSS -->
+        <link rel="stylesheet" href="../css/azia.css">
+        <style>
+          body, html {
+            height: 100%;
+            margin: 0;
+          }
+          
+          .bg {
+            /* The image used */
+            background-image: url('img/EL_AMINE.jpg');
+          
+            /* Full height */
+            height: 100%; 
+           
+            /* Center and scale the image nicely */
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+          }
+          </style>
+    </head>
+    <body class="bg">
+    
+  
+    <div class="row">
+      <div class="col-lg-8">
+      </div>
+      <div class="col-lg-4">
+  
+    
+        <div class="az-signin-wrapper" style="position: absolute;">
+            <div class="az-card-signin">
+                <h1 class="az-logo"><span>My</span>Espace</h1>
+                <div class="az-signin-header">
+                    <h2>Connexion</h2>
+                    <h4>Veuillez Connecter</h4>
+                    
+                    <form method="POST" action="{{ route('login') }}">
+                      @csrf
 
-    <!-- azia CSS -->
-    <link rel="stylesheet" href="../css/azia.css">
+                      <div class="form-group row">
+                          {{-- <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-  </head>
-  <body class="az-body">
+                          <div class="col-md-6">
+                              <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus> --}}
 
-    <div class="az-signin-wrapper">
-      <div class="az-card-signin">
-        <h1 class="az-logo">az<span>i</span>a</h1>
-        <div class="az-signin-header">
-          <h2>Welcome back!</h2>
-          <h4>Please sign in to continue</h4>
+                              @error('email')
+                                  <span class="invalid-feedback" role="alert">
+                                      <strong>{{ $message }}</strong>
+                                  </span>
+                              @enderror
+                          </div>
+                      </div>
+                      
+                    <div class="form-group row">
+                      <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-          {{-- <form action="https://www.bootstrapdash.com/demo/azia/v1.0.0/template/index.html"> --}}
-            <form method="POST" action="{{ route('login') }}">
+                      <div class="col-md-6">
+                          <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-            <div class="form-group">
-              <label>Email</label>
-              <input type="text" class="form-control" placeholder="Enter your email" value="demo@bootstrapdash.com">
-            </div><!-- form-group -->
-            <div class="form-group">
-              <label>Password</label>
-              <input type="password" class="form-control" placeholder="Enter your password" value="thisisademo">
-            </div><!-- form-group -->
-            <button class="btn btn-az-primary btn-block">Sign In</button>
-          </form>
-        </div><!-- az-signin-header -->
-        <div class="az-signin-footer">
-          <p><a href="../reset">Forgot password?</a></p>
-          <p>Don't have an account? <a href="/register">Create an Account</a></p>
-        </div><!-- az-signin-footer -->
-      </div><!-- az-card-signin -->
-    </div><!-- az-signin-wrapper -->
+                          @error('email')
+                              <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                              </span>
+                          @enderror
+                      </div>
+                  </div>
 
-    <script src="../lib/jquery/jquery.min.js"></script>
-    <script src="../lib/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="../lib/ionicons/ionicons.js"></script>
+                  <div class="form-group row">
+                      <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-    <script src="../js/azia.js"></script>
-    <script>
-      $(function(){
-        'use strict'
+                      <div class="col-md-6">
+                          <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
-      });
-    </script>
-  </body>
+                          @error('password')
+                              <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                              </span>
+                          @enderror
+                      </div>
+                  </div>
+                  <div class="form-group row mb-0">
+                    <div class="col-md-8 offset-md-4">
+                        <button type="submit" class="btn btn-primary">
+                            {{ __('Login') }}
+                        </button>
 
-<!-- Mirrored from www.bootstrapdash.com/demo/azia/v1.0.0/template/page-signin.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 06 Dec 2019 10:24:09 GMT -->
+                        @if (Route::has('password.request'))
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                        @endif
+                    </div>
+                </div>
+                    </form>
+                    <hr>
+<p>Don't have an account?</p>
+<a href="signup" class="btn btn-success">Inscription via mon Espace</a>
+<a href="https://enrolment.supinfo.com/NewCandidate.aspx?lang=fr" class="btn btn-success" target="_blank">Inscription via SupCom</a>
+
+                  </div><!-- az-signin-header -->
+   
+
+</div><!-- az-card-signin -->
+</div><!-- az-signin-wrapper -->
+</div>
+</div>
+
+<script src="lib/jquery/jquery.min.js"></script>
+<script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="lib/ionicons/ionicons.js"></script>
+
+<script src="js/azia.js"></script>
+</body>
 </html>
 
 
