99c49438bea1aa84af2cf47bd23962efc7cb4635
houssembenjemia <houssembenjemia@gmail.com>
2019-12-06 22:46:51 +0100

    event

--- backend/resources/views/home.blade.php
+++ backend/resources/views/home.blade.php
@@ -1,31 +1,9 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app') --}}
 
-@section('content')
+{{-- @section('content') --}}
 @extends('welcome')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in {{ Auth::user()->name }}
-
-                </div>
-               
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
 {{-- @extends('welcome')
 
 @section('core')
